8921b7e21ef8bd83080c1a8d868f755139a09c70
AnuragKushwaha4 <anuragjnv4@gmail.com>
2026-02-01 22:54:47 +0530

    Authentication Done

---
+++ Controllers/AuthController.js
@@ -0,0 +1,41 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const usermodel = require("../Models/usermodel");
+const userpost = require("../Models/userpost");
+
+
+async function Register(req,res){
+    let {name,username,email,password,age}=req.body;
+
+    let user = await usermodel.findOne({email:email});
+    if(user)return res.status(500).render(register);
+
+    bcrypt.genSalt(10,(err,salt)=>{
+        bcrypt.hash(password,salt,async (err,result)=>{
+            let user = await usermodel.create({
+                name,username,age,email,password:result
+            })
+        })
+    })
+
+    res.render("Login");
+}
+
+async function Login(req,res){
+    let {email,password}=req.body;
+    let loginuser = await usermodel.findOne({email:email,});
+
+    if(!loginuser)return res.status(500).render("Login");
+
+    bcrypt.compare(password,loginuser.password,async (err,result)=>{
+        if(!result)return res.status(500).render("Login");
+
+        let token = jwt.sign({email,password},"secret");
+
+        res.cookie("token",token);
+        let posts=await userpost.find().populate("user");       
+         res.render("first",{posts,user});
+    })
+
+}
+modules.exports ={Register,Login};
