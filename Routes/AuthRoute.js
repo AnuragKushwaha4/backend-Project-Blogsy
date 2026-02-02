2486eea01267e7a67a35876d60e50aa9341474d6
AnuragKushwaha4 <anuragjnv4@gmail.com>
2026-02-02 22:02:21 +0530

    AuthRoutesCompleted

--- Routes/AuthRoute.js
+++ Routes/AuthRoute.js
@@ -1,7 +1,14 @@
 const express = require("express");
-
+const {Register,Login,Logout}=require("../Controllers/AuthController")
 
 const router = express.Router();
 
 
+router.post("/register",Register);
+
+router.post("/login",Login);
+router.post("/logout",Logout)
+
+
+module.exports = router;
 
